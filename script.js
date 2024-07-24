1342c96e7091eae94e3bd2d964eff359790788e1
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 03:11:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function createTable() {
     //Write your code here
-  
+	var rows = prompt("Input number of rows");
+	var cols = prompt("Input number of columns");
+	var table = document.getElementById("myTable");
+	table.innerHTML = "";
+	for (var i = 0; i < rows; i++) {
+    var row = table.insertRow();
+    for (var j = 0; j < cols; j++) {
+      var cell = row.insertCell();
+      cell.innerHTML = "Row-" + i + " Column-" + j;
+    }
+  }
 }
